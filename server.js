0b16029315e45d20b97a5216e2a6d4e6b87290ad
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-04-27 11:59:06 -0400

    Set up server with basic crud action

--- server.js
+++ server.js
@@ -19,9 +19,59 @@
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    // defin model =============================================================
-    var Todo = congoose.model('Todo', {
+    // define model =============================================================
+    var Todo = mongoose.model('Todo', {
       text : String
+    });
+
+    //routes ===================================================================
+
+    // telling the app to get all todos
+    app.get('/api/todos', function(req, res) {
+      // use mongoose to get all the todos in the database
+      Todo.find(function(err, todos) {
+        // error handling
+        if (err)
+          res.send(err)
+        // telling the app to retunr all the todos as JSON
+        res.json(todos);
+      });
+    });
+
+    //create a todo and send back all todos, once created
+    app.post('/api/todos', function(req, res) {
+
+      //create a todo - info will come form AJAX requet form Angular
+      Todo.create({
+        text : req.body.text,
+        done : flase
+      }, function(err, todo) {
+        if (err)
+          res.send(err)
+        //this will return all the todos after create
+        Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+          res.json(todos);
+        });
+      });
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+      Todo.remove({
+        _id : req.params.todo_id
+      }, function(err, todo) {
+        if (err)
+          res.send(err);
+
+        Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+
+          res.json(todos);
+        });
+      });
     });
 
     // listen (start app with node server.js) ======================================
